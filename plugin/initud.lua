6b2c685df86ee306a375b2ec4889911aa110f6aa
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 14:48:53 +0200

    new version

--- plugin/initud.lua
+++ plugin/initud.lua
@@ -1,1 +1,1 @@
-os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q ident || exit 1 && cp ../nvim/init.* ~/.config/nvim/")
+os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/")
