317adb807df56291b5df11a154f9902a7e30fef5
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 15:56:12 +0200

    same as last one

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,4 +1,8 @@
-function initud()
+local function initud()
 	os.execute("echo $(pwd) > /tmp/paath")
 	--os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/
 end
+
+return {
+	initud = initud
+}
