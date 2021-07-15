a3002e99620ed9a82aae6db0e958ddfcfede153c
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 15:12:43 +0200

    actually add the command

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,1 +1,3 @@
-os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/")
+local function initud()
+	os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/")
+end
