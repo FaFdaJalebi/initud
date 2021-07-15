47890c7a7a34004e9fe3974e3c50e1ac62f843b6
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 18:55:03 +0200

    use absolute path

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,5 +1,5 @@
 local function initud()
-	os.execute("cp -v -u ./nvim/init.* ~/.config/nvim/")
+	os.execute("cp -v -u ~/.local/share/nvim/site/pack/paqs/start/initud/nvim/init.* ~/.config/nvim/")
 end
 
 return {
