46a858387ac2b287d6b6970590b7a5923e5239da
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 15:33:01 +0200

    fix typo

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,4 +1,4 @@
 function initud()
-	os.exectute("echo $(pwd) > /tmp/paath")
+	os.execute("echo $(pwd) > /tmp/paath")
 	--os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/
 end
