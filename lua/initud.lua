75f9c899f17970940bd1de55fb30942761e351e8
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 15:19:05 +0200

    do what the last commit was intended to do

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,4 +1,4 @@
 local function initud()
-	os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && echo $(pwd) > /tmp/paath/")
-	--cp ../nvim/init.* ~/.config/nvim/
+	os.exectute("echo $(pwd) > /tmp/paath")
+	--os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/
 end
