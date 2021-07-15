e25e30f3abcab46d8928dc4f76bd24823f1837eb
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-15 15:15:13 +0200

    debugging commit so  I can figure out the path

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,3 +1,4 @@
 local function initud()
-	os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && cp ../nvim/init.* ~/.config/nvim/")
+	os.execute("diff ../nvim/init.* ~/.config/nvim/init.* -s | grep -q diff || exit 1 && echo $(pwd) > /tmp/paath/")
+	--cp ../nvim/init.* ~/.config/nvim/
 end
