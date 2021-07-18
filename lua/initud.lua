9da9a1772c19394da1b594d3215eec5b08c24825
CorruptedVor <51972476+CorruptedVor@users.noreply.github.com>
2021-07-18 12:23:15 +0200

    add deoplete-jedi to system automatically

--- lua/initud.lua
+++ lua/initud.lua
@@ -1,5 +1,6 @@
 local function initud()
 	os.execute("cp -v -u ~/.local/share/nvim/site/pack/paqs/start/initud/nvim/init.* ~/.config/nvim/")
+	os.execute("pip3 install --user jedi --upgrade")
 end
 
 return {
